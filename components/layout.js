f78f2e44e4d7400501f3dffa895bb745e3d451c9
HerbCaudill <herb@devresults.com>
2021-01-24 14:13:00 +0100

    components/layout.js:

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Herb Caudill'
+const name = 'Herb Caudill!'
 export const siteTitle = 'Herb Caudill | Blog'
 
 export default function Layout({ children, home }) {
